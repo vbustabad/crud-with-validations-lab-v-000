b7b6165ebed6c1f4ecae9671020d934ec72f5f98
Veronica Bustabad <vbustabad@gmail.com>
2018-08-10 02:37:14 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,5 @@
 class Song < ActiveRecord::Base
+<<<<<<< HEAD
   validates :title, presence: true, uniqueness: { scope: [:artist_name, :release_year], message: "The title cannot be repeated by the same artist in the same year." }
   validates :released, inclusion: { in: [true, false] }
 
@@ -11,6 +12,20 @@ class Song < ActiveRecord::Base
 
   def is_the_song_released?
     released
+=======
+  @song = Song.new
+
+  validates :title, presence: true
+  validates :released, inclusion: { in: [true, false] }
+  validates :release_year, presence: true, if :released?
+  validate :release_year_cannot_be_in_the_future
+  validates :artist_name, presence: true
+
+  def release_year_cannot_be_in_the_future
+    if release_year.present? && release_year ≤ Date.today
+      errors.add(:release_year, "The release year cannot be in the future.")
+    end
+>>>>>>> efe7e774c3f64f5e695c7b626a6c630b877ffc00
   end
 
 end
